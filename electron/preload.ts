1175b0def0b313b8a522099128a4a29bc83f9e99
Ido Lavi <idolav9@gmail.com>
2025-11-14 23:40:36 +0200

    Open Editor logic implementation

--- electron/preload.ts
+++ electron/preload.ts
@@ -6,5 +6,6 @@ contextBridge.exposeInMainWorld("electronAPI", {
     ipcRenderer.invoke("check-editor-installed", editorName),
   chooseProjectDirectory: () => ipcRenderer.invoke("os/choose-project-directory"),
   getGitInfo: (projectPath: string) => ipcRenderer.invoke("git/get-info", projectPath),
+  openProjectInEditor: (editorName: string, projectPath: string) =>
+    ipcRenderer.invoke("editor/open-project", editorName, projectPath),
 });
-
